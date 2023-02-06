5e296a4330ecf3ebf48d804a5e3aae833c5a6f32
Abhijeet <abhigulati1000@gmail.com>
2023-02-07 00:23:31 +0530

    Daily Challenge Feb 6

--- February/Feb6.java
+++ February/Feb6.java
@@ -1,10 +1,26 @@
+
+
 //1470 Shuffle the array
 /*Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 
 Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 */
 
-
+import java.util.*;
 public class Feb6 {
-    
+    public int[] shuffle(int[] nums, int n) {
+        //2*n=nums.length
+    ArrayList<Integer> ans=new ArrayList<>();
+    int i=0;
+    while(i<n){
+        ans.add(nums[i]);
+        ans.add(nums[n+i]);
+        i++;
+    }
+    int[] arr=new int[nums.length];
+    for(int j=0;j<ans.size();j++){
+        arr[j]=ans.get(j);
+    }
+    return arr;
+    }
 }
