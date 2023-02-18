2a84c228122f2f9e32b4585dd7602742b5750171
Abhijeet <abhigulati1000@gmail.com>
2023-02-18 19:50:04 +0530

    Daily challenge

---
+++ February/Feb18.java
@@ -0,0 +1,28 @@
+public class Feb18 {
+     public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+ }
+        public TreeNode invertTree(TreeNode root) {
+        //base case
+            if(root==null){
+                return null;
+            }
+            //traversal 
+            TreeNode left=invertTree(root.left);
+            TreeNode right=invertTree(root.right);//now we have reached the children of the root node now we will make the swap or interchange the values of the node
+            root.left=right;
+            root.right=left;
+            return root;
+                    
+        }
+    
+}
