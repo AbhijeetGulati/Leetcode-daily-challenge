1b15372f8810480449e8129e3a322faf81da368c
Abhijeet <abhigulati1000@gmail.com>
2023-02-26 22:03:23 +0530

    Daily Challenge Feb 24

--- February/Feb24.java
+++ February/Feb24.java
@@ -1,3 +1,18 @@
+//1675. Minimize Deviation in Array
+/*
+ * You are given an array nums of n positive integers.
+
+You can perform two types of operations on any element of the array any number of times:
+
+If the element is even, divide it by 2.
+For example, if the array is [1,2,3,4], then you can do this operation on the last element, and the array will be [1,2,3,2].
+If the element is odd, multiply it by 2.
+For example, if the array is [1,2,3,4], then you can do this operation on the first element, and the array will be [2,2,3,4].
+The deviation of the array is the maximum difference between any two elements in the array.
+
+Return the minimum deviation the array can have after performing some number of operations.
+ */
+
 import java.util.Collections;
 import java.util.PriorityQueue;
 
