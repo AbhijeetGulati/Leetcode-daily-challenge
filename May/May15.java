393456713f07c13a0ac2271b9b6957c5dcb75bd7
Abhijeet <abhigulati1000@gmail.com>
2023-05-20 16:08:25 +0530

    Daily Challenge MAY 15

--- May/May15.java
+++ May/May15.java
@@ -10,6 +10,13 @@ Return the head of the linked list after swapping the values of the kth node fro
 package May;
 
 public class May15 {
+    public class ListNode {
+             int val;
+             ListNode next;
+             ListNode() {}
+             ListNode(int val) { this.val = val; }
+             ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+         }
     public ListNode swapNodes(ListNode head, int k) {
         if(head==null || head.next==null){
             return head;
