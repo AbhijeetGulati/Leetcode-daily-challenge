bf87fe65ce6549943ccf8c2dd1fc298487048a30
Abhijeet <abhigulati1000@gmail.com>
2023-05-20 16:11:34 +0530

    changes in May 16 Daily Challenge

--- May/May16.java
+++ May/May16.java
@@ -3,16 +3,17 @@
 Given a linked list, swap every two adjacent nodes and return its head. 
 You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.) */
 
-public class ListNode {
-         int val;
-         ListNode next;
-         ListNode() {}
-         ListNode(int val) { this.val = val; }
-         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     }
 
+package May;
 
 class Solution { 
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     public ListNode swapPairs(ListNode head) {
         //edge case
         if(head==null || head.next==null){
